5e5c9e588b0cfb65e7fed03d79596b70c0b9e215
JaiThakur10 <dev10jai@gmail.com>
2024-04-23 14:54:42 +0530

    avatar

--- src/app/(app)/UserDashboard/page.tsx
+++ src/app/(app)/UserDashboard/page.tsx
@@ -70,6 +70,9 @@ function UserDashboard() {
 
   return (
     <div className="my-8 mx-4 md:mx-8 lg:mx-auto p-6 bg-white rounded w-full max-w-6xl">
+        <div className=' h-20 w-20 rounded-full bg-gray-100 p-1'>
+            <img width={200}  height={200} src="https://api.dicebear.com/8.x/lorelei/svg?seed=Oreo" alt="avatar" />
+        </div>
       <h1 className="text-4xl font-bold mb-4">Hey {`${username}`}! Welcome to the User Dashboard</h1>
       
     </div>
