e57e732d5b612d75b30d7468e60e71875d5a463c
JaiThakur10 <dev10jai@gmail.com>
2024-04-23 14:30:27 +0530

    authrization done

---
+++ src/app/(app)/UserDashboard/page.tsx
@@ -0,0 +1,79 @@
+'use client';
+
+
+import { Button } from '@/components/ui/button';
+import { Separator } from '@/components/ui/separator';
+import { Switch } from '@/components/ui/switch';
+import { useToast } from '@/components/ui/use-toast';
+
+import { ApiResponse } from '@/types/ApiResponse';
+import { zodResolver } from '@hookform/resolvers/zod';
+import axios, { AxiosError } from 'axios';
+import { Loader2, RefreshCcw } from 'lucide-react';
+import { User } from 'next-auth';
+import { useSession } from 'next-auth/react';
+import React, { useCallback, useEffect, useState } from 'react';
+import { useForm } from 'react-hook-form';
+
+
+function UserDashboard() {
+
+
+  const { toast } = useToast();
+
+  
+
+  const { data: session } = useSession();
+
+  const form = useForm({
+    
+  });
+
+  const { register, watch, setValue } = form;
+
+
+  const fetchAcceptMessages = useCallback(async () => {
+   
+    try {
+      const response = await axios.get<ApiResponse>('/api/accept-messages');
+      
+    } catch (error) {
+      const axiosError = error as AxiosError<ApiResponse>;
+      toast({
+        title: 'Error',
+        description:
+          axiosError.response?.data.message ??
+          'Failed to fetch message settings',
+        variant: 'destructive',
+      });
+    } 
+  }, [setValue, toast]);
+
+ 
+
+  // Fetch initial state from the server
+
+
+  // Handle switch change
+ 
+
+  if (!session || !session.user) {
+    return <div></div>;
+  }
+
+  const { username } = session.user as User;
+
+  const baseUrl = `${window.location.protocol}//${window.location.host}`;
+  const profileUrl = `${baseUrl}/u/${username}`;
+
+  
+
+  return (
+    <div className="my-8 mx-4 md:mx-8 lg:mx-auto p-6 bg-white rounded w-full max-w-6xl">
+      <h1 className="text-4xl font-bold mb-4">Hey {`${username}`}! Welcome to the User Dashboard</h1>
+      
+    </div>
+  );
+}
+
+export default UserDashboard;
---
+++ src/app/(auth)/verify/[username]/page.tsx
@@ -0,0 +1,100 @@
+'use client';
+
+import { Button } from '@/components/ui/button';
+import {
+  Form,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from '@/components/ui/form';
+import { Input } from '@/components/ui/input';
+import { useToast } from '@/components/ui/use-toast';
+import { ApiResponse } from '@/types/ApiResponse';
+import { zodResolver } from '@hookform/resolvers/zod';
+import axios, { AxiosError } from 'axios';
+import { useParams, useRouter } from 'next/navigation';
+import { useForm } from 'react-hook-form';
+import * as z from 'zod';
+import { verifySchema } from '@/schemas/verifySchema';
+import { useState } from 'react';
+import { Loader2 } from 'lucide-react';
+
+export default function VerifyAccount() {
+  const [isSubmitting, setIsSubmitting] = useState(false);
+  const router = useRouter();
+  const params = useParams<{ username: string }>();
+  const { toast } = useToast();
+  const form = useForm<z.infer<typeof verifySchema>>({
+    resolver: zodResolver(verifySchema),
+  });
+
+  const onSubmit = async (data: z.infer<typeof verifySchema>) => {
+    setIsSubmitting(true);
+    try {
+      const response = await axios.post<ApiResponse>(`/api/verify-code`, {
+        username: params.username,
+        code: data.code,
+      });
+
+      toast({
+        title: 'Success',
+        description: response.data.message,
+      });
+
+      router.replace('/sign-in');
+    } catch (error) {
+      const axiosError = error as AxiosError<ApiResponse>;
+      toast({
+        title: 'Verification Failed',
+        description:
+          axiosError.response?.data.message ??
+          'An error occurred. Please try again.',
+        variant: 'destructive',
+      });
+    }
+    finally{
+        setIsSubmitting(false)
+    }
+  };
+
+  return (
+    <div className="flex justify-center items-center h-[630px] bg-gray-100">
+      <div className="w-full max-w-md p-8 space-y-8 bg-white rounded-lg shadow-md">
+        <div className="text-center">
+          <h1 className="text-4xl font-extrabold tracking-tight lg:text-5xl mb-6">
+            Verify Your Account
+          </h1>
+          <p className="mb-4">Enter the verification code sent to your email</p>
+        </div>
+        <Form {...form}>
+          <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6">
+            <FormField
+              name="code"
+              control={form.control}
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel className=' ml-[130px]'>Verification Code</FormLabel>
+                  <Input {...field}
+                  placeholder='Enter your verification Code'
+                  />
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <Button className='bg-gradient-to-br relative group/btn from-black dark:from-zinc-900 dark:to-zinc-900 to-neutral-600  dark:bg-zinc-800 w-full text-white rounded-md h-10 font-medium shadow-[0px_1px_0px_0px_#ffffff40_inset,0px_-1px_0px_0px_#ffffff40_inset] dark:shadow-[0px_1px_0px_0px_var(--zinc-800)_inset,0px_-1px_0px_0px_var(--zinc-800)_inset]' type="submit">
+            {isSubmitting ? (
+                <>
+                  <Loader2 className="mr-2 h-4 w-4 animate-spin " />
+                 Verifying
+                </>
+              ) : (
+                'Verify'
+              )}
+            </Button>
+          </form>
+        </Form>
+      </div>
+    </div>
+  );
+}
